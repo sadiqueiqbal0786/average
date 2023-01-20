f924e072ecf7ab0aacc356892aeddd1e16ec0d18
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-20 21:54:41 +0530

    chal nikal

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -6,6 +6,19 @@ public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Use Scanner methods to read input
+        int n=0;
+        int num;
+        int avg=0;
+        int round = 0;
+        while(true){
+            num=scanner.nextInt();
+            n++;
+            if(num==0) break;
+            avg+=num;
+            round= (int) Math.ceil(avg/n);
+        }
+        System.out.print(round);
+
 
     }
 
